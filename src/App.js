4721ac852e0cb7934757caf778f408cafea1f545
AbhishekkGautam <abhishekgautm243@gmail.com>
2021-07-23 17:59:28 +0530

    style: add images

--- src/App.js
+++ src/App.js
@@ -6,51 +6,62 @@ var allBooks = {
     {
       title: "Deep Work",
       author: "Cal Newport",
-      rating: "9/10",
+      rating: "4/5",
+      image: "https://m.media-amazon.com/images/I/417yjF+Z5zL.jpg",
     },
     {
       title: "Atomic Habits",
       author: "James Clear",
-      rating: "9.5/10",
+      rating: "4.6/5",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
     },
     {
       title: "Getting Things Done",
       author: "David Allen",
-      rating: "8/10",
+      rating: "4/5",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/51KNBNziJXL._SX327_BO1,204,203,200_.jpg",
     },
   ],
   Startup: [
     {
       title: "Zero To One",
       author: "Peter Thiel",
-      rating: "9/10",
+      rating: "4.3/5",
+      image: "https://images-na.ssl-images-amazon.com/images/I/71m-MxdJ2WL.jpg",
     },
     {
       title: "The Lean Startup",
       author: "Eric Ries",
-      rating: "8/10",
+      rating: "4/5",
+      image: "https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg",
     },
     {
       title: "Rework",
       author: "Jason Fried",
-      rating: "9/10",
+      rating: "4.2/5",
+      image: "https://images-na.ssl-images-amazon.com/images/I/61GFc+k-5PL.jpg",
     },
   ],
   Biography: [
     {
       title: "Wings of Fire",
       author: "Dr. Abdul Kalam and Arun Tiwari",
-      rating: "10/10",
+      rating: "4.8/5",
+      image: "https://3.imimg.com/data3/VM/WT/MY-12313319/kalam-500x500.jpeg",
     },
     {
       title: "Elon Musk",
       author: "Ashlee Vance",
-      rating: "10/10",
+      rating: "4.6/5",
+      image: "https://m.media-amazon.com/images/I/5179mrE+WWL.jpg",
     },
     {
       title: "Steve Jobs",
       author: "Walter Isaacson",
-      rating: "9/10",
+      rating: "4/5",
+      image: "https://images-na.ssl-images-amazon.com/images/I/81VStYnDGrL.jpg",
     },
     // {
     //   title: "The Story of My Life",
@@ -62,17 +73,22 @@ var allBooks = {
     {
       title: "Eloquent JavaScript",
       author: "Marijn Haverbeke",
-      rating: "10/10",
+      rating: "4.8/5",
+      image: "https://images-na.ssl-images-amazon.com/images/I/91asIC1fRwL.jpg",
     },
     {
       title: "Clean Code",
       author: "Robert Cecil Martin",
-      rating: "9/10",
+      rating: "4/5",
+      image:
+        "https://images-eu.ssl-images-amazon.com/images/I/41SH-SvWPxL._SX342_SY445_QL70_ML2_.jpg",
     },
     {
-      title: "You Don't Know JS",
-      author: "Kyle Simpson",
-      rating: "9/10",
+      title: "JavaScript - The Good Parts",
+      author: "Douglas Crockford",
+      rating: "4.6/5",
+      image:
+        "https://images-na.ssl-images-amazon.com/images/I/5131OWtQRaL._SX381_BO1,204,203,200_.jpg",
     },
   ],
 };
@@ -121,13 +137,16 @@ export default function App() {
             <ul>
               {bookList.map((book) => (
                 <li key={book.title}>
-                  {" "}
-                  <div style={{ fontSize: "larger" }}> {book.title} </div>
-                  <div style={{ fontSize: "smaller", marginTop: "0.2rem" }}>
-                    {" "}
-                    {book.author}{" "}
+                  <div className="img-container">
+                    <img src={book.image} alt="book" />
                   </div>
-                  <div style={{ marginTop: "0.7rem" }}> {book.rating} </div>
+                  <div className="book-content">
+                    <div style={{ fontSize: "larger" }}> {book.title} </div>
+                    <div style={{ fontSize: "smaller", marginTop: "0.2rem" }}>
+                      {book.author}
+                    </div>
+                    <div style={{ marginTop: "0.7rem" }}> {book.rating} </div>
+                  </div>
                 </li>
               ))}
             </ul>
